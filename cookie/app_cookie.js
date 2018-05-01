689d8bd1eab273057c4a7bc92e03aaa7fb0b9984
ImAready <lycm37@gmail.com>
2018-05-01 13:30:18 +0900

    shopping cart 2

--- cookie/app_cookie.js
+++ cookie/app_cookie.js
@@ -22,6 +22,29 @@ app.get('/products', function(req, res){
     res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">Cart</a>`);
 });
 
+/*
+cart = {
+    제품 아이디 : 제품 수량
+}
+*/
+app.get('/cart/:id', function(req, res){
+    var id = req.params.id;
+    // res.send('Hi'+id);
+    if(req.cookies.cart){ //cookie가 있을 때
+        var cart = req.cookies.cart;
+    } else {
+        var cart = {};
+    }
+
+    if(!cart[id]){ // 새로운 제품이면 0으로 초기화
+        cart[id] = 0;
+    } 
+    cart[id] = parseInt(cart[id]) + 1; //cookie return string 
+    res.cookie('cart', cart);
+    // res.send(cart);
+    res.redirect('/cart');
+});
+
 app.get('/count', function(req, res){
     if(req.cookies.count){
         var count = parseInt(req.cookies.count);
