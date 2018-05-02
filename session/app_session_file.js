04ba7a6775f30d334f89628b8f3c1f64a48e61a1
ImAready <lycm37@gmail.com>
2018-05-02 13:25:44 +0900

    session file store

---
+++ session/app_session_file.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const session = require('express-session'); 
+const bodyParser = require('body-parser');
+//session을 file에 저장
+const fileStore = require('session-file-store')(session); // express-session과 의존관계 ->인자로 넘겨줌
+
+
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(session({
+    secret: '1231임의13415값asdfasdf~#!@',
+    resave: false,
+    saveUninitialized: true,
+    store: new fileStore() //사용자의 session data를 파일에 저장.
+}));
+
+app.get('/count', function(req, res){
+    // req.session.count = 1;
+    // res.send('Hi');
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send('count : '+req.session.count);
+});
+
+app.get('/temp', function(req, res){
+    res.send('result : '+req.session.count)
+});
+
+app.get('/auth/login', function(req, res){
+    var output = '';
+    output = `
+    <h1>Login</h1>
+        <form action="/auth/login" method="post">
+            <p>
+                <input type="text" name="username" placeholder="username">
+            </p>
+            <p>
+                <input type="text" name="password" placeholder="password">
+            </p>
+            <p>
+                <input type="submit">
+            </p>
+        </form>
+    `;
+    
+    res.send(output);
+});
+app.post('/auth/login', function(req, res){
+    var user = {
+      username:'hello',
+      password:'111',
+      displayName:'Master'
+    };
+
+    var uname = req.body.username;
+    var pwd = req.body.password;
+
+    if(uname === user.username && pwd === user.password){
+      req.session.displayName = user.displayName;
+      res.redirect('/welcome');
+    } else {
+      res.send('Who are you? <a href="/auth/login">login</a>');
+    }
+});
+
+app.get('/welcome', function(req, res){
+    if(req.session.displayName) {
+      res.send(`
+        <h1>Hello, ${req.session.displayName}</h1>
+        <a href="/auth/logout">logout</a>
+      `);
+    } else {
+      res.send(`
+        <h1>Welcome</h1>
+        <a href="/auth/login">Login</a>
+      `);
+    }
+});
+
+app.get('/auth/logout', function(req, res){
+    delete req.session.displayName;
+    res.redirect('/welcome');
+});
+
+app.listen(3003, function(){
+    console.log('Connected 3003 port...');
+});
