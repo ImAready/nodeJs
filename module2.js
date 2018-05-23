a264df819927878b19f6e808d19f2c6f83b99d9b
ImAready <lycm37@gmail.com>
2018-05-23 14:19:17 +0900

    create module2

--- module2.js
+++ module2.js
@@ -1,3 +1,7 @@
 var sum = require('./lib/sum');
 // require가 module.exports의 값인 함수로 치환됨. 
-console.log(sum(1, 2));
+console.log('sum : ',sum(1, 2));
+
+var calculator = require('./lib/calculator');
+console.log('cal sum : ',calculator.sum(1,2));
+console.log('cal avg : ',calculator.avg(1,2));
