efb0358dca37f8230f94ae9729419f1b4178f989
ImAready <lycm37@gmail.com>
2018-05-23 14:08:05 +0900

    jade extends2

--- jade_extends.js
+++ jade_extends.js
@@ -14,3 +14,9 @@ app.get('/add', function(req, res){
 app.listen(3003, function(){
     console.log('Connected 3003 Port...');
 })
+
+/*jade
+| : text로 인식
+extends : 탬플릿 상속
+block content : content라는 block을 호출
+*/ 
